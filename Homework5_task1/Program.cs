e1b3fd3f2329ce608dc7a1a2728fe716f2b025c5
Artem Davydov <ethrik@yandex.ru>
2023-01-26 23:36:56 +0800

    Task 1 and 2 ready

---
+++ Homework5_task1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework5_task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string defaultString = "Пожалуйста, введите предложение, с любыми знаками препинания! (Если не хотите - нажмите Enter.)";
+            Console.WriteLine(defaultString);
+            string inputString = Console.ReadLine().Trim();
+
+            PrintWords(inputString.Length == 0 ? SplitText(defaultString) : SplitText(inputString) );
+
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Разделяет строку на слова
+        /// </summary>
+        /// <param name="str">Предложение со знаками препинания</param>
+        /// <returns>Массив строк-слов</returns>
+        static string[] SplitText(string str)
+        {
+            char[] separators = new char[] { ' ', ',', '.', '!', '?', '-', ':', ';', '(', ')', '"' };
+            string[] words = str.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+            return words;
+        }
+
+        /// <summary>
+        /// Выводит слова через строку
+        /// </summary>
+        /// <param name="words">Список строк-слов</param>
+        static void PrintWords(string[] words)
+        {
+            for (int i = 0; i < words.Length; i++)
+            {
+                Console.WriteLine(words[i]);
+            }
+        }
+    }
+}
---
+++ Homework5_task2/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Homework5_task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string defaultString = "Пожалуйста введите список слов разделенных пробелами";
+            Console.WriteLine(defaultString);
+            string inputString = Console.ReadLine();
+
+            Console.WriteLine(inputString.Trim().Length == 0 ? Reverse(defaultString) : Reverse(inputString));
+
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Возвращает строку с переставленными задом наперёд словами
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        static string Reverse(string text)
+        {
+            string[] textSplitted = SplitText(text);
+            //запоминаем кол-во слов
+            int wordsCount = textSplitted.Length;
+            //eсли text - одно слово или пусто, сразу вернем его
+            if (wordsCount <= 1)
+            {
+                return text;
+            }
+            //опустошаем аргумент
+            text = "";
+            //делаем первое слово с маленькой буквы, а последнее - с большой
+            string firstWord = textSplitted[wordsCount - 1];
+            string lastWord = textSplitted[0];
+            if (!Char.IsUpper(firstWord[0]))
+            {
+                text = firstWord.Substring(0, 1).ToUpper() + firstWord.Substring(1, firstWord.Length - 1) + " ";
+            }
+            else
+            {
+                text = firstWord + " ";
+            }
+            if (!Char.IsLower(lastWord[0]))
+            {
+                lastWord = lastWord.Substring(0, 1).ToLower() + lastWord.Substring(1, lastWord.Length - 1);
+            }
+            for (int i = wordsCount - 2; i > 0; i--)
+            {
+                text += textSplitted[i] + " ";
+            }
+
+            return text += lastWord;
+        }
+
+        /// <summary>
+        /// Разделяет строку на слова
+        /// </summary>
+        /// <param name="str">Предложение со знаками препинания</param>
+        /// <returns>Массив строк-слов</returns>
+        static string[] SplitText(string str)
+        {
+            char[] separators = new char[] { ' ', ',', '.', '!', '?', '-', ':', ';', '(', ')', '"' };
+            string[] words = str.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+            return words;
+        }
+    }
+}
+
